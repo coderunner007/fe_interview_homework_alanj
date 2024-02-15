20cd00e43aee6e77660cabe85346dabd2f12526c
Alan Joseph <alanj@localhost.localdomain>
2024-02-16 01:20:34 +0530

    Basic UI layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import Timeline from './lib/Timeline.svelte';
+	import LinkSidebar from './lib/LinkSidebar.svelte';
 </script>
 
-<Timeline />
+<main class="grid grid-cols-[50px_1fr]">
+	<LinkSidebar />
+	<Timeline />
+</main>
---
+++ src/lib/LinkSidebar.svelte
@@ -0,0 +1,24 @@
+<style lang="postcss">
+	.logo {
+		color: #8b55dd;
+		display: block;
+		flex: none;
+		background: currentColor;
+		mask-size: contain;
+		mask-repeat: no-repeat;
+		mask-position: center;
+		width: 25px;
+		height: 25px;
+		margin: 10px;
+		mask-image: url('../assets/logo-symbol.svg');
+	}
+</style>
+
+<nav id="sidebar" class="flex flex-col items-center bg-purple-950">
+	<a
+		class="logo"
+		href="https://plan.toggl.com"
+		target="_blank"
+		rel="noopener noreferrer">
+	</a>
+</nav>
